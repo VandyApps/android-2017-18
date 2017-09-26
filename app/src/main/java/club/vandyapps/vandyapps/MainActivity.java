20bb3d578019d8b4b7f4aa7ef02ccdf257ed1752
Patrick <patrickkerrypei@users.noreply.github.com>
2017-09-26 16:46:17 -0500

    Progress from lesson 1 (09-19-17)

---
+++ app/src/main/java/club/vandyapps/vandyapps/MainActivity.java
@@ -0,0 +1,28 @@
+package club.vandyapps.vandyapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1, button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Feedback", Toast.LENGTH_LONG).show();
+            }
+
+        });
+    }
+}
