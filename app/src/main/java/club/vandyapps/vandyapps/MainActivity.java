06974d12e9d19f0f1cf56bc2e789ef0a932d7acf
Patrick <patrickkerrypei@users.noreply.github.com>
2017-09-26 20:08:26 -0500

    Lesson 2 09-26-17

--- app/src/main/java/club/vandyapps/vandyapps/MainActivity.java
+++ app/src/main/java/club/vandyapps/vandyapps/MainActivity.java
@@ -1,28 +1,43 @@
 package club.vandyapps.vandyapps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button button1, button2;
+
+    private Button feedbackButton;
+    private Button changeActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
 
-        button1 = (Button) findViewById(R.id.button);
-        button2 = (Button) findViewById(R.id.button2);
+        feedbackButton = (Button) findViewById(R.id.feedback);
+        changeActivityButton = (Button) findViewById(R.id.changeActivity);
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        feedbackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Feedback", Toast.LENGTH_LONG).show();
             }
 
         });
+        changeActivityButton.setOnClickListener(new OurListener());
+    }
+
+    public class OurListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, Wrapper.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/club/vandyapps/vandyapps/Wrapper.java
@@ -0,0 +1,29 @@
+package club.vandyapps.vandyapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Wrapper extends AppCompatActivity {
+
+    private static final String VANDYVAS_URL = "https://vandyvans.com/m/routes";
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+
+        webView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl(VANDYVAS_URL);
+    }
+}
