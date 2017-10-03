abafb1e3f6327f554f16d66da74c55078c8c6458
Patrick <patrickkerrypei@users.noreply.github.com>
2017-10-03 16:09:05 -0500

    Add permission request

--- app/src/main/java/club/vandyapps/vandyapps/Wrapper.java
+++ app/src/main/java/club/vandyapps/vandyapps/Wrapper.java
@@ -1,8 +1,17 @@
 package club.vandyapps.vandyapps;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -12,6 +21,7 @@ import android.widget.Toast;
 public class Wrapper extends AppCompatActivity {
 
     private static final String VANDYVANS_URL = "https://vandyvans.com/m/routes";
+    private static final int VV_PERMISSION_ACCESS_FINE_LOCATION = 1;
     private WebView webView;
 
     @Override
@@ -19,11 +29,74 @@ public class Wrapper extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.webview);
 
+        requestLocationPermission();
+
         webView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+        webSettings.setGeolocationEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
 
         webView.loadUrl(VANDYVANS_URL);
+    }
+
+    private void requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Title")
+                        .setMessage("Msg")
+                        .setPositiveButton("positive", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(Wrapper.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        VV_PERMISSION_ACCESS_FINE_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        VV_PERMISSION_ACCESS_FINE_LOCATION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String permissions[],
+            @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case VV_PERMISSION_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission. ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        webView.reload();
+                    }
+                } else {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Permission denied but crucial to app",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
